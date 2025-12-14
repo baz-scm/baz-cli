1a9681ef3082250d86b22b747605874f7d219d17
nimrodkor <nimrod@baz.co>
2025-12-15 00:02:15 +0200

    CR

--- src/components/PullRequestReview.tsx
+++ src/components/PullRequestReview.tsx
@@ -112,10 +112,10 @@ const PullRequestReview: React.FC<PullRequestReviewProps> = ({
 
     return {
       unmetRequirements: latestSpecReview.requirements.filter(
-        (req) => req.verdict !== "Met",
+        (req) => req.verdict.toLowerCase() !== "met",
       ),
       metRequirements: latestSpecReview.requirements.filter(
-        (req) => req.verdict === "Met",
+        (req) => req.verdict.toLowerCase() === "met",
       ),
     };
   };
